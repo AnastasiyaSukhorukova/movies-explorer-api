09d39b5c841cf769eec11c3fe91b19a251005f43
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-06-12 12:04:27 +0300

    добавление мидлевры авторизации

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+/* eslint-disable consistent-return */
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const AuthError = require('../errors/authError');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    return next(new AuthError('Необходима авторизация (ошибка токена)'));
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  return next(); // пропускаем запрос дальше
+};
