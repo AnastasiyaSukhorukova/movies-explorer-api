8e7e9d783c889f94a0eea435379e7a3c7e0ef848
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-06-14 23:55:10 +0300

    добавила корс

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const errorsMiddleware = require('./middlewares/errors');
 const auth = require('./middlewares/auth');
@@ -8,6 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 app.use(requestLogger);
 
 app.use(express.json());
